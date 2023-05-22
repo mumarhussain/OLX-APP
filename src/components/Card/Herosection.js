d0a7a69cb90d010b69e4045bee5f653476c2b953
mumarhussain <mumarhussain126@gmail.com>
2023-05-22 18:16:15 +0500

    some changes

--- src/components/Card/Herosection.js
+++ src/components/Card/Herosection.js
@@ -1,45 +1,48 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useState, useEffect } from "react";
 import { db } from "@/config/firebase";
-import { deleteUser } from "../../store/slice/Userslice";
 import { collection, getDocs } from "firebase/firestore";
+import { doc, deleteDoc , updateDoc} from "firebase/firestore";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Card1 from "./Card1";
 import dayjs from "dayjs";
 import "dayjs/locale/en";
 import Product from "../Product";
-import Navbar from '../Navbar';
+import Navbar from "../Navbar";
 dayjs.extend(relativeTime);
 
 function Herosection() {
-  const dispatch = useDispatch();
   const [selectedItem, setSelectedItem] = useState(null);
-  const [fetchData, setFetchData] = useState([]); 
-  const data = useSelector((state) => state.user.dataForm);
+  const [fetchData, setFetchData] = useState([]);
   const login = useSelector((state) => state.user.loginUser);
   const user = useSelector((state) => state.user.userData);
   const userId = login.id;
 
-  const handleClick = (id) => { };
+  const handleClick = (id) => {};
 
-  const handleDelete = (id) => {
-    dispatch(deleteUser(id));
+  const handleDelete = async (id) => {
+  await  deleteDoc(doc(db, "formData",id));
   };
 
-  const handleEdit = (id) => {
+  const handleEdit = async (id) => {
+    const updataData = doc(db, "formData", id)
+    await updateDoc(updataData , {
+      id,
+      
+      
+    })
     setSelectedItem(id);
-  };
-  // console.log(fetchData, "setFetchData");
 
+  };
   useEffect(() => {
     const fetchForms = async () => {
       try {
         const snapShot = await getDocs(collection(db, "formData"));
-        const fetchedData = snapShot.docs.map((doc) =>{
-          if("forms" in doc.data())
-          setFetchData(doc.data().forms);
-
-          });
+        const fetchedDatas = snapShot.docs.map((doc) => {
+          return doc.data()
+        });
+        setFetchData(fetchedDatas);
+        console.log("fetchData", fetchedDatas);
       } catch (error) {
         console.log(error, "error");
       }
@@ -47,28 +50,33 @@ function Herosection() {
 
     fetchForms();
   }, []);
-  return (<>
-    <Navbar className=""/>
-    <div className="bg-white">
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-around space-x-2 mx-3">
-        <Card1 />
-        <Card1 />
-        <Card1 />
-        <Card1 />
+  return (
+    <>
+      <Navbar className="" />
+      <div className="bg-white">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-around space-x-2 mx-3">
+          <Card1 />
+          <Card1 />
+          <Card1 />
+          <Card1 />
+        </div>
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-around space-x-2 mx-3  ">
+          {fetchData.map((item, index) => {
+            return (
+              <Product
+                key={index}
+                userId={userId}
+                handleClick={handleClick}
+                item={item}
+                handleEdit={handleEdit}
+                handleDelete={handleDelete}
+              />
+            );
+          })}
+        </div>
       </div>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-around space-x-2 mx-3  ">
-        {fetchData.map((item,index) => {
-          return (
-            <Product key={index} userId={userId} handleClick={handleClick} item={item} handleEdit={handleEdit} handleDelete={handleDelete} />
-          )
-        })}
-      </div>
-    </div>
     </>
   );
 }
 
 export default Herosection;
-
-
-
--- src/pages/formsData/[id]/index.js
+++ src/pages/formsData/[id]/index.js
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import Header from "../../../components/Header";
 import { addUser, editUser, loginUser } from "../../../store/slice/Userslice";
 import { db } from "@/config/firebase";
-import { collection, addDoc } from "firebase/firestore"
+import { collection, addDoc, doc, setDoc } from "firebase/firestore"
 
 function FormsData() {
   const dispatch = useDispatch();
@@ -14,11 +14,9 @@ function FormsData() {
   const userData = useSelector((state) =>
     state.user.dataForm.find((userData) => userData.id == queryId)
   );
-  const forms = useSelector((state)=>state.user.dataForm)
   const login = useSelector((state) => state.user.loginUser);
   const id = userData ? userData.id : Math.floor(Math.random() * 1000000);
   const [details, setDetails] = useState();
-
   useEffect(() => {
     setDetails({
       adtitle: userData ? userData.adtitle : "",
@@ -59,8 +57,14 @@ function FormsData() {
       router.push("/");
     } 
     try {
-      // await addDoc(collection(db, "users"), login);
-      await addDoc(collection(db, "formData"), {forms});
+      let myDet = { ...details}
+      delete myDet.id
+      const data =  doc(collection(db, "formData"))
+      const allData = await setDoc(data, {
+        ...myDet,
+        id: data.id
+      })
+      console.log("sadasdasda", allData)
     } catch (error) {
       console.log(error, "error");
     }
@@ -75,92 +79,8 @@ function FormsData() {
         id: queryId,
       })
     );
-    router.push("/");
+    router.push("/",details);
   };
-
-
-
-
-// import React, { useEffect, useState } from "react";
-// import Link from "next/link";
-// import { useRouter } from "next/router";
-// import { useDispatch, useSelector } from "react-redux";
-// import Header from "../../../components/Header";
-// import { addUser, editUser, loginUser } from "../../../store/slice/Userslice";
-// import { db } from "@/config/firebase";
-// import { collection, addDoc, getDocs } from "firebase/firestore";
-
-// function FormsData() {
-//   const dispatch = useDispatch();
-//   const router = useRouter();
-//   const queryId = router.query.id;
-//   const userData = useSelector((state) =>
-//     state.user.dataForm.find((userData) => userData.id == queryId)
-//   );
-//   const forms = useSelector((state) => state.user.dataForm);
-//   const login = useSelector((state) => state.user.loginUser);
-//   const id = userData ? userData.id : Math.floor(Math.random() * 1000000);
-//   const [details, setDetails] = useState();
-
-//   useEffect(() => {
-//     setDetails({
-//       adtitle: userData ? userData.adtitle : "",
-//       description: userData ? userData.description : "",
-//       price: userData ? userData.price : "",
-//       name: userData ? userData.name : "",
-//       address: userData ? userData.address : "",
-//       number: userData ? userData.number : "",
-//       id: id,
-//       userId: login?.id,
-//       image: userData ? userData.image : "",
-//       createdAt: userData ? userData.createdAt : new Date().toString(),
-//     });
-//   }, [userData]);
-
-  
-
-//   const handleChange = (event) => {
-//     const { name, value } = event.target;
-//     setDetails((prev) => {
-//       return { ...prev, [name]: value };
-//     });
-//   };
-
-//   const handleClick = async (event) => {
-//     event.preventDefault();
-//     if (
-//       details.adtitle === "" ||
-//       details.description === "" ||
-//       details.price === "" ||
-//       details.name === "" ||
-//       details.address === "" ||
-//       details.number === "" ||
-//       details.image === "" ||
-//       details.id === "" ||
-//       details.userId === ""
-//     ) {
-//       alert("Please fill out all the fields");
-//     } else {
-//       dispatch(addUser(details));
-//       router.push("/");
-//     }
-//     try {
-//       await addDoc(collection(db, "formData"), { forms });
-//     } catch (error) {
-//       console.log(error, "error");
-//     }
-//   };
-
-//   const handleUpdate = (event) => {
-//     event.preventDefault();
-//     dispatch(
-//       editUser({
-//         ...details,
-//         id: queryId,
-//       })
-//     );
-//     router.push("/");
-//   };
 
   return (
     <>
